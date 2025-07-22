7c3da61b434d67160bee0c83b4d3d9fb4f178c1d
KirillKnyshenko <kirillknyshenko063@gmail.com>
2025-07-22 18:00:58 +0300

    Optimitaion
    Сейчас удалил колонку chatID из price_history. Ищу теперь только уникальные артикулы, не повторяются запросы по одинаковым артикулам для разных пользователей

--- Main.py
+++ Main.py
@@ -3,9 +3,18 @@ from Transform import transform
 from Load import load_price_data
 
 chatIds = get_chat_ids()
-
+articles = []
+print(chatIds)
 for chatId in chatIds:
-    articles = get_articles(chatId)
-    prices = extract(articles)
-    csv_path = transform(prices, chatId)
-    load_price_data(csv_path)
+    articles_list_db = get_articles(chatId)
+    for art in articles_list_db:
+        articles.append(art)
+
+# Удаляю дубли артикулов
+print(articles)
+articles = list(set(articles))
+print(articles)
+
+prices = extract(articles)
+csv_path = transform(prices)
+load_price_data(csv_path)
--- Transform.py
+++ Transform.py
@@ -5,14 +5,13 @@ import os
 import tempfile
 
 
-def transform(prices_dict: dict, chat_id: int) -> str:
+def transform(prices_dict: dict) -> str:
     try:
         print(prices_dict, type(prices_dict))
         df = pd.DataFrame(prices_dict)
 
         df["dateUpdate"] = datetime.now()
         df["operationDate"] = datetime.now()
-        df["chatId"] = chat_id
 
         temp_dir = tempfile.mkdtemp()
         temp_path = os.path.join(temp_dir, "prices.csv")
